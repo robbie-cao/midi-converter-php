74cbb845932bc033c059020aaf30ffca14d3ad1e
Robbie Cao <robbie.cao@gmail.com>
2016-12-09 18:03:34 +0800

    Format code
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- midi2xml.php
+++ midi2xml.php
@@ -3,52 +3,43 @@ $show_help = false;
 $p['tt'] = 1;	# default to delta time
 $file = $argv[$argc-1]; # filename is always last argument
 
-if (in_array($argv[1], array('--help', '-help', '-h', '-?')))
-{ $show_help = true; }
+if (in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
+    $show_help = true;
+}
 
-if (! file_exists($file))
-{
+if (!file_exists($file)) {
     echo "File not found.\n";
     exit();
 }
 
-foreach ($argv as $arg)
-{
-    if (strpos($arg, 'timestamp') != null)
-    {
+foreach ($argv as $arg) {
+    if (strpos($arg, 'timestamp') != null) {
         $values = split('=', $arg);
 
         if ($values[1] == 'delta') {
             $p['tt'] = 1;
-        }
-        elseif ($values[1] == 'absolute') {
+        } elseif ($values[1] == 'absolute') {
             $p['tt'] = 0;
-        }
-        else {
+        } else {
             $show_help = true;
             break;
         }
     }
 }
 
-if ($show_help)
-{
+if ($show_help) {
     echo "Help...\n";
     exit();
 }
 
-$tt = isset($p['tt'])?$p['tt']:0;
+$tt = isset($p['tt']) ? $p['tt'] : 0;
 
-If ($file!='') {
+if ($file != '') {
     require('midi.class.php');
 
     $midi = new Midi();
     $midi->importMid($file);
     $xml = $midi->getXml($tt);
-}
-
-if ($file != '')
-{
     echo $xml;
 }
 
